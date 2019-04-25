48c6e28725390e04668f47152d2bc954b252272f
DavidGrunheidt <davidordine@hotmail.com>
2019-04-25 18:11:43 -0300

    Determination of FA algorithm

--- src/actions/Fsm.js
+++ src/actions/Fsm.js
@@ -1,6 +1,9 @@
+import Fsm from "../model/Fsm"
+
 export const FSM_EDIT = 'FSM_EDIT';
 
 //FSM
 export function fsmEdit(fsm) {
+    console.log(fsm.determine());
     return { type: FSM_EDIT, payload: fsm };
 }
