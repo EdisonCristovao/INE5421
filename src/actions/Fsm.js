41dc89736bb6f906e050a23d37381edd925586a7
DavidGrunheidt <davidordine@hotmail.com>
2019-04-26 13:21:21 -0300

    Part of determination algorithm done

--- src/actions/Fsm.js
+++ src/actions/Fsm.js
@@ -4,6 +4,6 @@ export const FSM_EDIT = 'FSM_EDIT';
 
 //FSM
 export function fsmEdit(fsm) {
-    console.log(fsm.determine());
+    fsm.determine();
     return { type: FSM_EDIT, payload: fsm };
 }
--- src/model/fsm/Determinator.js
+++ src/model/fsm/Determinator.js
@@ -1,29 +1,79 @@
 import Fsm from "../Fsm";
 import { EPSILON } from "../SymbolValidator";
+import FSM from "../Fsm";
+import { FSM_EDIT } from "../../actions/Fsm";
 
 export function isDeterministic(fsm) {
   if (!fsm instanceof Fsm) return "NotStance";
 
-  let hasMuitTransition = false;
-  let hasEpsilon = false;
-  hasMuitTransition = fsm.transitions.some(
+  return !(
+    hasMultTransitions(fsm.transitions) || 
+    hasEpsilon(fsm.alphabet)
+    );
+}
+
+function hasMultTransitions(transitions) {
+  return transitions.some(
     tran => tran.to.split(",").length > 1 //>  1 isDeterministic is false
   );
+}
 
-  hasEpsilon = fsm.alphabet.some(
+function hasEpsilon(alphabet) {
+  return alphabet.some(
     letter => letter === EPSILON //>  1 isDeterministic is false
   );
-
-  return !(hasMuitTransition || hasEpsilon);
 }
 
 export function determine(fsm) {
   let isDet = isDeterministic(fsm);
-  if (isDet == "NotStance") return "NotStance";
-  else if(isDet ) return "AlreadyDeterministic";
+  if (isDet === "NotStance") return "NotStance";
+  else if(isDet) return "AlreadyDeterministic";
 
-  let detFSM = fsm.clone();
-  
+  if (!hasEpsilon(fsm.alphabet)) 
+    return determineWithoutEpsilon(fsm);
+  else 
+    return determineWithEpsilon(fsm);
+}
 
-  return detFSM;
+function determineWithoutEpsilon(fsm) {
+  let fsmDet = new FSM();
+  let nStates0 = 0;
+  let nStates1 = 1;
+  let possibleStates = [];
+  let realStates = new Set();
+
+  fsmDet.initial = fsm.initial;
+  fsmDet.states.push(fsm.initial);
+  fsmDet.alphabet = fsm.alphabet;
+
+  fsm.alphabet.forEach(
+    symbol => possibleStates.push(new Set())
+  );
+
+  while(nStates0 != nStates1) {
+    fsmDet.states[nStates0].split(",").forEach(
+      singleState => {
+        fsm.transitions.forEach(
+          transition => {
+            if (transition.from === singleState)
+              transition.to.split(",").forEach(
+                state => possibleStates[
+                  fsm.alphabet.indexOf(transition.when)].
+                  add(state)
+              ); 
+          }
+        );
+      }
+    );
+    
+
+    possibleStates.forEach(posState => {
+      // Coisas
+      posState.clear();
+    });
+    realStates.clear();
+    break;
+  }
+
+  return fsmDet;
 }
