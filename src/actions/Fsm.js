91744c16d6a9c7aa04af1b024f7e7c6611ca1861
DavidGrunheidt <davidordine@hotmail.com>
2019-04-28 23:29:58 -0300

    Deterministic done

--- src/actions/Fsm.js
+++ src/actions/Fsm.js
@@ -4,6 +4,5 @@ export const FSM_EDIT = 'FSM_EDIT';
 
 //FSM
 export function fsmEdit(fsm) {
-    fsm.determine();
     return { type: FSM_EDIT, payload: fsm };
 }
--- src/model/fsm/Determinator.js
+++ src/model/fsm/Determinator.js
@@ -13,9 +13,12 @@ export function isDeterministic(fsm) {
 }
 
 function hasMultTransitions(transitions) {
-  return transitions.some(
-    tran => tran.to.split(",").length > 1 //>  1 isDeterministic is false
-  );
+  return transitions.some(tran => {
+    if (tran.to !== undefined && tran.to !== "")  
+      return tran.to.split(",").length > 1 //>  1 isDeterministic is false
+    else
+      return false
+  });
 }
 
 function hasEpsilon(alphabet) {
@@ -24,60 +27,87 @@ function hasEpsilon(alphabet) {
   );
 }
 
-export function determine(fsm) {
-  let isDet = isDeterministic(fsm);
-  if (isDet === "NotStance") return "NotStance";
-  else if(isDet) return "AlreadyDeterministic";
-
-  if (!hasEpsilon(fsm.alphabet)) 
-    return determineWithoutEpsilon(fsm);
-  else 
-    return determineWithEpsilon(fsm);
-}
-
-function determineWithoutEpsilon(fsm) {
+// This function acts like a class
+function makeDeterministic(fsm) {
   let fsmDet = new FSM();
+  let eStar = [];
   let actualState = 0;
   let possibleStates = [];
 
-  // First state will be the first of the original FA
-  fsmDet.initial = fsm.initial;
-  fsmDet.states.push(fsm.initial);
-  fsmDet.alphabet = fsm.alphabet;
+  let search = function(eStarIndex, originalState, auxState) {
+    if (auxState === originalState) return;
+    if (eStar[eStarIndex].has(auxState)) return;
 
-  // Auxiliar array to collect all states that a symbol leads
-  for(let i = 0; i < fsm.alphabet.length; i++)
-    possibleStates.push(new Set());
+    eStar[eStarIndex].add(auxState);
 
-  /* 
-   * Colect all states that, throught a state (new state
+    let auxTrans = fsm.transitions.filter(
+      trans => trans.from === auxState &&
+        trans.when === EPSILON && trans.to !== undefined
+    );
+
+    if (auxTrans[0] === undefined) return;
+
+    auxTrans[0].to.split(",").forEach(state =>
+      search(eStarIndex, originalState, state)
+    );
+  }
+
+  let eStarCalculate = function() {
+    if (!hasEpsilon(fsm.alphabet)) return;
+
+    fsm.transitions.forEach(
+      transition => {
+        if (transition.when === EPSILON &&
+          transition.to !== undefined) {
+          transition.to.split(",").forEach(
+            state => {
+              search(fsm.states.indexOf(transition.from),
+                transition.from, state);
+            }
+          )
+        }
+      }
+    );
+  }
+
+
+  /*
+   * Collects all states that, throught a state (new state
    * in the new automata), with a symbol, whe can go to
    */
-  while(actualState != fsmDet.states.length) {
-    
+  let calculatePossibleStates = function() {
     fsmDet.states[actualState].split(",").forEach(
       singleState => {
         fsm.transitions.forEach(
           transition => {
             if (transition.from === singleState
-              && transition.to !== undefined) {
+              && transition.to !== undefined
+              && transition.to !== ""
+              && transition.when !== EPSILON) {
               transition.to.split(",").forEach(
-                state => possibleStates[
-                  fsm.alphabet.indexOf(transition.when)]
-                  .add(state)
-              ); 
+                state => {
+
+                  eStar[fsm.states.indexOf(state)]
+                    .forEach(eStarState => possibleStates[
+                      fsmDet.alphabet.indexOf(transition.when)]
+                      .add(eStarState)
+                    );
+                }
+              )
             }
           }
-        );
+        )
       }
     );
-    
-    /*
-     * Sorts all states collected in the previous loop
-     * to make sure that there's no equivalent states
-     * before adding new states and transitions. 
-     * (for example, (A,B) or (B,A) are the same thing).
-     */
+  }
+
+  /*
+   * Sorts all states collected in the previous loop
+   * to make sure that there's no equivalent states
+   * before adding new states and transitions.
+   * (for example, (A,B) or (B,A) are the same thing).
+   */
+  let calculateRealStates = function() {
     possibleStates.forEach(
       (state, index) => {
         let fromStateStr = fsmDet.states[actualState];
@@ -88,25 +118,68 @@ function determineWithoutEpsilon(fsm) {
 
         fsmDet.transitions.push(
           {
-            "from": fromStateStr ,
+            "from": fromStateStr,
             "to": toStateStr,
             "when": fsmDet.alphabet[index]
           }
         );
-        
+
         /* 
          * Makes sure that the new state
          * isn't on states list before adding it.
          */
         if (!fsmDet.states.includes(toStateStr))
           fsmDet.states.push(toStateStr);
-        
+
         // Set context variables for next iteration
         state.clear();
       }
     );
+  }
 
+  let calculateFinalStates = function() {
+    fsmDet.states.forEach(state => {
+      if (state.split(",").some(singleState =>
+        fsm.finals[fsm.states.indexOf(singleState)])) {
+        fsmDet.finals.push(true);
+      } else {
+        fsmDet.finals.push(false);
+      }
+    });
+  }
+  // Initializes eStar array
+  fsm.states.forEach(state => {
+    if (state !== undefined && state !== "")
+      eStar.push(new Set([state]))
+  });
+
+  // Alphabet is the same from the original (Without epsilon)
+  fsmDet.alphabet = fsm.alphabet.filter(a => a != EPSILON);
+
+  // Auxiliary array to collect all states that a symbol leads
+  for (let i = 0; i < fsmDet.alphabet.length; i++)
+    possibleStates.push(new Set());
+
+  eStarCalculate();
+
+  // First state will be e* of the original FA first state
+  fsmDet.initial = Array.from(eStar[0]).sort().join(",");
+  fsmDet.states.push(fsmDet.initial);
+
+  while(actualState !== fsmDet.states.length) {
+    calculatePossibleStates();
+    calculateRealStates();
     actualState++;
   }
+  calculateFinalStates();
+  
   return fsmDet;
+}
+
+export function determine(fsm) {
+  let isDet = isDeterministic(fsm);
+  if (isDet === "NotStance") return "NotStance";
+  else if(isDet) return "AlreadyDeterministic";
+
+  return makeDeterministic(fsm);
 }
