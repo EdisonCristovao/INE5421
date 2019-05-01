266202748fd05933508253ce70bd7dd05d0e4990
Edison Cristovao <edison.cr.jr@gmail.com>
2019-05-01 15:56:45 -0300

    insert img

--- src/app/routes/Welcome/index.js
+++ src/app/routes/Welcome/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import IntlMessages from 'util/IntlMessages';
+import React from "react";
+import { Link } from "react-router-dom";
+import IntlMessages from "util/IntlMessages";
 
 const Welcome = () => (
-    <div className="app-wrapper page-error-container animated slideInUpTiny animation-duration-3">
-        <div className="page-error-content">
-            <div className="error-code mb-4 animated zoomInDown">
-                <img src="assets/images/error.png"/>
-            </div>
-            <h2 className="text-center fw-regular title bounceIn animation-delay-10 animated">
-                <IntlMessages id="extraPages.404Msg"/>
-            </h2>
-            {/* <form className="mb-4" role="search">
+  <div className="app-wrapper page-error-container animated slideInUpTiny animation-duration-3">
+    <div className="page-error-content">
+      <div className=" mb-4 animated zoomInDown">
+        <img src="assets/images/excited-Woman.png" style={{ width: "100%" }} />
+      </div>
+      <h2 className="text-center fw-regular title bounceIn animation-delay-10 animated mb-0">
+        Bem vindo
+      </h2>
+      <h1 className="text-center fw-regular title bounceIn animation-delay-15 animated mt-0">
+        Fm.io
+      </h1>
+      {/* <form className="mb-4" role="search">
                 <div className="search-bar flipInX animation-delay-16 animated">
                     <div className="form-group">
                         <input type="search" className="form-control form-control-lg border-0"
@@ -22,11 +25,13 @@ const Welcome = () => (
                     </div>
                 </div>
             </form> */}
-            <p className="text-center zoomIn animation-delay-20 animated">
-                <Link className="btn btn-primary" to="/app/dashboard">Dashboard</Link>
-            </p>
-        </div>
+      <p className="text-center zoomIn animation-delay-20 animated">
+        <Link className="btn btn-primary" to="/app/dashboard">
+          Dashboard
+        </Link>
+      </p>
     </div>
+  </div>
 );
 
 export default Welcome;
