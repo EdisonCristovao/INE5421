828a2252100c9bfdf91bfd7e50db16d6a7c8afc8
DavidGrunheidt <davidordine@hotmail.com>
2019-05-10 14:50:55 -0300

    Helper done

--- src/containers/SideNav/ModalHelp.js
+++ src/containers/SideNav/ModalHelp.js
@@ -27,19 +27,27 @@ class Modalhelp extends React.Component {
                 >
                     <ModalHeader toggle={this.toggleModal}>Help FM-IO</ModalHeader>
                     <ModalBody>
-                        <h2>Definindo um automato</h2>
-                        <p>Defina o alfabeto em <strong>lowerCase</strong>, em seguida defina as transições estados em <strong>UperCase</strong>.<br/>
-                        Transição nao deterministica deve se usar virgula ex: <strong>(A,B)</strong>.<br/>
-                        Caso nao tenha transição pode deixar em branco. <br/>
-                        Epsilon é representado por {`${'&'}`}. </p>
+                        <h2><strong>Definindo um automato</strong></h2>
+                        <p>Na primeira linha adicionam-se letras ao <strong>alfabeto</strong> do automato.<br/>
+                        Nas linhas seguintes adicionam-se os <strong> estados</strong> (1º coluna) e suas <strong>transições</strong> (demais colunas).<br/> 
+                        Para adicionar uma nova letra ao alfabeto, basta clicar <strong>'ENTER'</strong> na ultima coluna da primeira linha.<br/>
+                        Para adicionar um novo estado, basta clicar no botão <strong>"Add"</strong>.<br/>
+                        Para limpar o automato, basta clicar no botão <strong>"limpar"</strong>.<br/>
+                        Transição nao deterministica deve-se usar virgula, ex: <strong>A,B</strong>.<br/>
+                        Transição para estado morto são representadas por transições em branco. <br/>
+                        Epsilon é representado por "<strong>{`${'&'}`}"</strong>. </p>
 
-                        <h2>Definindo uma Gramatica</h2>
-                        <p>As produções são geradas como o ex: <strong>S -> aS | a </strong> </p>
+                        <h2><strong>Definindo uma Gramatica</strong></h2>
+                        <p>As produções são do tipo: <strong>S -> aS | a </strong><br/> 
+                        A <strong>primeira</strong> produção adicionada contem o simbolo inicial da gramatica.<br/>
+                        Ao escrever uma gramatica, simbolos terminais e não terminais <strong>devem ambos</strong> ter só um caracter.<br/>
+                        Ou seja, simbolos não terminais do tipo "a^" ou terminais do tipo "q0" não serão aceitos.<br/></p>
 
-                        <h2>Reconhecimento de senteças</h2>
-                        <p>Basta escrever a sentença e precionar 'ENTER', a cor mostra se foi reconhecido ou não. </p>
+                        <h2><strong>Reconhecimento de senteças</strong></h2>
+                        <p>Basta escrever a sentença e precionar <strong>'ENTER'</strong>. <br/>
+                        Caso cor = verde, reconheceu. Caso cor = vermelho, não reconheceu.</p>
 
-                        <h2>Espressão regular</h2>
+                        <h2><strong>Expressão regular</strong></h2>
                         <p><strong>Breve...</strong> </p>
                     </ModalBody>
                     <ModalFooter>
